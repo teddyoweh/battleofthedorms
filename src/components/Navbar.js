066355b2fa5a4e7af6913ace1b736064bc7ccbe4
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2023-01-19 06:44:00 -0600

    udd

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(true);
   return (
   <nav>
     {/* <a href="/">
@@ -22,6 +23,20 @@ const Navbar = () => {
 
 
         <Link to="/register">Register</Link>
+      </div>
+    </div>
+    <div className="mrp">
+      <div onClick={()=>setIsOpen(!isOpen)} className="menu-btn">
+      <i class='bx bx-menu'></i>
+      </div>
+      <div className={isOpen==false?"hide":"menu-box"}>
+
+      <div className="items">
+        <Link to="/">Home</Link>
+        <Link to="/"> Schedule</Link>
+        <Link to="/">Leaderboard</Link>
+        <Link to="/">Media</Link>
+    </div>
       </div>
     </div>
 
